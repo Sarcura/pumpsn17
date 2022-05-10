6f0e7ace2f1fa2eb8f54c491e069680400dc9a96
Georg Auer <georg.auer@sarcura.com>
2022-05-10 10:04:03 +0200

    new todos, debian linux program

--- files/gui.py
+++ files/gui.py
@@ -1,3 +1,7 @@
+
+# TODO: calculate total needed fluids. create buttons for faster forward/back of single pumps
+# TODO: set sw endstops to better values HINT: slight increase
+
 import dearpygui.dearpygui as dpg
 from themes import create_theme_imgui_light, create_theme_client, create_theme_server
 from arduino import Arduino
